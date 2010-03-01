e34ac4ab782a3701cb0dede43ffec30fc448359d
Tony Cook <tony@develop-help.com>
2010-03-01 08:27:17 +0000

    update to trace through POE::Kernel functions instead of writing directly to the handle (which is no longer available)

--- poexs.c
+++ poexs.c
@@ -77,12 +77,40 @@ Return: void
 =cut
 */
 
+#ifdef XS_LOOP_TRACE
+
+static int trace_initialized;
+
+static int trace_files;
+static int trace_events;
+static int trace_calls;
+static int trace_statistics;
+
+static void
+do_trace_initialize(void);
+
+#define TRACE_INITIALIZE() if (!trace_initialized) do_trace_initialize()
+
+#endif
+
 void
 poe_enqueue_data_ready(SV *kernel, int mode, int *fds, int fd_count) {
   dSP;
   int i;
 
-  POE_TRACE_CALL(("<cl> poe_enqueue_data_ready(mode %d (%s)", mode, poe_mode_names(mode)));
+#ifdef XS_LOOP_TRACE
+  TRACE_INITIALIZE();
+
+  if (trace_calls) {
+    SV *sv = newSVpvf("<cl> poe_enqueue_data_ready(mode %d (%s)", mode, poe_mode_names(mode));
+    for (i = 0; i < fd_count; ++i) {
+      sv_catpvf(sv, ", %d", fds[i]);
+    }
+    sv_catpv(sv, ")\n");
+    poexs_trace_call("%s", SvPV_nolen(sv));
+    SvREFCNT_dec(sv);
+  }
+#endif
 
   ENTER;
   SAVETMPS;
@@ -91,10 +119,8 @@ poe_enqueue_data_ready(SV *kernel, int mode, int *fds, int fd_count) {
   PUSHs(sv_2mortal(newSVsv(kernel)));
   PUSHs(sv_2mortal(newSViv(mode)));
   for (i = 0; i < fd_count; ++i) {
-    POE_TRACE_CALL((", %d", fds[i]));
     PUSHs(sv_2mortal(newSViv(fds[i])));
   }
-  POE_TRACE_CALL((")\n"));
   PUTBACK;
 
   perl_call_method("_data_handle_enqueue_ready", G_DISCARD);
@@ -291,13 +317,6 @@ poe_trap(const char *fmt, ...) {
 
 #ifdef XS_LOOP_TRACE
 
-static int trace_initialized;
-
-static int trace_files;
-static int trace_events;
-static int trace_calls;
-static int trace_statistics;
-
 static int
 get_trace_flag(const char *name) {
   dSP;
@@ -336,11 +355,6 @@ do_trace_initialize(void) {
   trace_initialized = 1;
 }
 
-static PerlIO *
-get_trace_file(void) {
-  return IoIFP(sv_2io(sv_2mortal(newSVpv("POE::Kernel::TRACE_FILE", 0))));
-}
-
 /*
 =head1 TRACE MACROS
 
@@ -377,43 +391,76 @@ Update a statistic, controlled by the POE TRACE_STATISTICS constant.
 void
 poexs_trace_file(const char *fmt, ...) {
   va_list va;
+  dSP;
 
   if (!trace_initialized)
     do_trace_initialize();
   if (!trace_files)
     return;
 
+  ENTER;
+  SAVETMPS;
+  EXTEND(SP, 3);
+  PUSHMARK(SP);
   va_start(va, fmt);
-  PerlIO_vprintf(get_trace_file(), fmt, va);
+  PUSHs(sv_2mortal(vnewSVpvf(fmt, &va)));
   va_end(va);
+  PUTBACK;
+
+  perl_call_pv("POE::Kernel::_warn", G_DISCARD);
+
+  FREETMPS;
+  LEAVE;
 }
 
 void
 poexs_trace_event(const char *fmt, ...) {
   va_list va;
+  dSP;
 
   if (!trace_initialized)
     do_trace_initialize();
   if (!trace_events)
     return;
 
+  ENTER;
+  SAVETMPS;
+  EXTEND(SP, 3);
+  PUSHMARK(SP);
   va_start(va, fmt);
-  PerlIO_vprintf(get_trace_file(), fmt, va);
+  PUSHs(sv_2mortal(vnewSVpvf(fmt, &va)));
   va_end(va);
+  PUTBACK;
+
+  perl_call_pv("POE::Kernel::_warn", G_DISCARD);
+
+  FREETMPS;
+  LEAVE;
 }
 
 void
 poexs_trace_call(const char *fmt, ...) {
   va_list va;
+  dSP;
 
   if (!trace_initialized)
     do_trace_initialize();
   if (!trace_calls)
     return;
 
+  ENTER;
+  SAVETMPS;
+  EXTEND(SP, 3);
+  PUSHMARK(SP);
   va_start(va, fmt);
-  PerlIO_vprintf(get_trace_file(), fmt, va);
+  PUSHs(sv_2mortal(vnewSVpvf(fmt, &va)));
   va_end(va);
+  PUTBACK;
+
+  perl_call_pv("POE::Kernel::_warn", G_DISCARD);
+
+  FREETMPS;
+  LEAVE;
 }
 
 void

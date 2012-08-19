eb7ab57243265b793ed246cae8df2a619e4192af
Tony Cook <tony@develop-help.com>
2012-08-19 13:38:54 +1000

    1.002 release

--- lib/POE/XS/Loop/EPoll.pm
+++ lib/POE/XS/Loop/EPoll.pm
@@ -7,7 +7,7 @@ BEGIN {
     # noisy
     *POE::Kernel::TRACE_CALLS = sub () { 0 };
   }
-  $VERSION = '1.001_02';
+  $VERSION = '1.002';
   eval {
     # try XSLoader first, DynaLoader has annoying baggage
     require XSLoader;
